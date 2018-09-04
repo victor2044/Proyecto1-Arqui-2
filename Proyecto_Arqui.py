781fd808940140e71cc240fb00f190f93840e120
victor2044 <victor2044@hotmail.es>
2018-09-04 12:44:16 -0600

    Prueba de implementacion de la unidad de control

--- Proyecto_Arqui.py
+++ Proyecto_Arqui.py
@@ -30,30 +30,32 @@ class processor(threading.Thread):
         while i < 2:
             i+=1
             #aleat = randint(0,2)
-            aleat = 0
+            aleat = 1
 
-            if aleat == 0:
+            if aleat == 1:
                 self.write()
-                self.cache.monitoreo()
 
-            elif aleat == 1:
-                self.cache.monitoreo()
+            elif aleat == 0:
                 self.read()
+                
             else:
                 self.cache.monitoreo()
                 self.processing()
 
 class cache(threading.Thread):
     mem_cache = [(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0),(2,0)]
- 
+    global mutex
+    
     def __init__(self, name, control_unit, bus):
         threading.Thread.__init__(self)
         self.name = name
-        self.control = control_unit
+        self.control = control_unit (self.men_cache)
         self.bus = bus
         self.stoprequest = threading.Event()
         
     def write(self,pos,data):
+        mutex.acquire()
+        self.monitoreo()
         index = self.mem_cache[pos]
         msi = index[0]
         validate = self.control.write(msi)
@@ -62,6 +64,7 @@ class cache(threading.Thread):
             prot = [1,self.name,pos, data]
             time.sleep(2)
             bus.bus_princ(prot)
+            mutex.release()
             print ("escritura exitosa")
             print (self.mem_cache)
         elif validate == 1:
@@ -69,9 +72,11 @@ class cache(threading.Thread):
             time.sleep(2)
             prot = [0,self.name,pos, data]
             new_data = bus.bus_princ(prot)
-            mem_cache[pos] = (2,new_data[2])      
+            mutex.release()
+            self.mem_cache[pos] = (2,new_data[2])      
                    
     def read(self,pos):
+        self.monitoreo()
         index = self.mem_cache[pos]
         msi = index[0]
         validate = self.control.read(msi)
@@ -84,6 +89,7 @@ class cache(threading.Thread):
             time.sleep(2)
             prot = [0,self.name,pos, data]
             new_data = bus.bus_princ(prot)
+            mutex.release()
             self.mem_cache[pos] = (2,new_data[2])
             return new_data[2]
         else:
@@ -92,7 +98,6 @@ class cache(threading.Thread):
 
     def monitoreo(self):
         instr = self.control.snoop()
-        print ("estoy corriendo")
         print ("estoy aqui :")
         print (instr)
         if instr:
@@ -100,10 +105,10 @@ class cache(threading.Thread):
             data = index[1]
             if instr[0] == 0:
                 self.mem_cache[instr[2]] = (1,data)
-                print ("cache actualizada por lectura")
+                print ("cache actualizada por lectura" + self.name)
             else:
                 self.mem_cache[instr[2]] = (2,data)
-                print ("cache actualizada por escritura")
+                print ("cache actualizada por escritura" + self.name)
 
     def run (self):
         while True:
@@ -115,6 +120,7 @@ class control_unit(threading.Thread):
     def __init__(self, name, bus):
         threading.Thread.__init__(self)
         self.name = name
+        self.mem_cache = 
         self.bus = bus
         self.stoprequest = threading.Event()
 
@@ -219,24 +225,50 @@ class memory (threading.Thread):
     def run (self):
         while True:
             a=1
+
+mutex = threading.Lock()
             
-                    
+#hilos unicos                   
 princ_mem = memory("principal_memory")
 bus = bus("principal_bus", princ_mem)
+
+#Unidad de control
 control_unit1 = control_unit("1", bus)
 control_unit2 = control_unit("2", bus)
+control_unit3 = control_unit("3", bus)
+control_unit4 = control_unit("4", bus)
+
+#Caches
 cache1 = cache("1", control_unit1, bus)
 cache2 = cache("2", control_unit2, bus)
+cache3 = cache("3", control_unit3, bus)
+cache4 = cache("4", control_unit4, bus)
+
+#Cores
 core1 = processor("1",cache1)
 core2 = processor("2",cache2)
+core3 = processor("3",cache3)
+core4 = processor("4",cache4)
 
 princ_mem.start()
 bus.start()
+
 control_unit1.start()
 control_unit2.start()
+control_unit3.start()
+control_unit4.start()
+
 cache1.start()
 cache2.start()
+cache3.start()
+cache4.start()
+
 core1.start()
 core2.start()
+core3.start()
+core4.start()
 
 core1.join()
+core2.join()
+core3.join()
+core4.join()
