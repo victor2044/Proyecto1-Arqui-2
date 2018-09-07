d802f411b7e7ec4c916f08a85c5d32b2e0f63cc5
Victor <victor2044@hotmail.es>
2018-09-07 08:29:07 -0600

    Add files via upload
    Actualizacion de la interfaz debido a problemas de sincronizacion anteriormente

--- GUI_Arqui.py
+++ GUI_Arqui.py
@@ -1,160 +1,179 @@
 import tkinter as tk
-import threading
+from tkinter import *
+from threading import Thread
 import time
+from tkinter.scrolledtext import ScrolledText
 
-class GUI(threading.Thread):
- 
-   def __init__(self):
-      threading.Thread.__init__(self)
-      self.root = tk.Tk()
-      self.root.Core1 = tk.Text(self.root)
-      self.root.Core2 = tk.Text(self.root)
-      self.root.Core3 = tk.Text(self.root)
-      self.root.Core4 = tk.Text(self.root)
-      self.root.Memory = tk.Text(self.root)
-      self.root.Misses = tk.Text(self.root)
-      self.root.Cache1 = tk.Text(self.root)
-      self.root.Cache2 = tk.Text(self.root)
-      self.root.Cache3 = tk.Text(self.root)
-      self.root.Cache4 = tk.Text(self.root)
+ventana = tk.Tk()
+ventana.title("Modelo de protocolo para coherencia de cache en sistemas multiprocesador")
+ventana.geometry("1430x600")
+ventana.resizable(width=False, height=False)
+ventana.configure(bg = "black")
 
-      self.p1 = "Core 1 Action \n"
-      self.p2 = "Core 2 Action \n"
-      self.p3 = "Core 3 Action \n"
-      self.p4 = "Core 4 Action \n"
-      self.m1 = "Cache 1"
-      self.m2 = "Cache 2"
-      self.m3 = "Cache 3"
-      self.m4 = "Cache 4"
+label1= Label(ventana,text="Processors",fg = "Yellow",font=("Helvetica", 14),bg = "black")
+label1.place(y=5, x=680,width=100, height=20)
+      
+label2= Label(ventana,text="Cache Memory",fg = "Yellow",font=("Helvetica", 14),bg = "black")
+label2.place(y=240, x=515,width=150, height=20)
 
+label3= Label(ventana,text="Cache Misses",fg = "Yellow",font=("Helvetica", 14),bg = "black")
+label3.place(y=240, x=1115,width=150, height=20)
+
+label4= Label(ventana,text=" Principal Memory",fg = "Yellow",font=("Helvetica", 14),bg = "black")
+label4.place(y=475, x=650,width=180, height=20)
+
+Canva1 = Canvas(ventana)
+Canva1.place(y=30, x=0, width=350, height=200)
+
+Canva2 = Canvas(ventana)
+Canva2.place(y=30, x=360, width=350, height=200)
+
+Canva3 = Canvas(ventana)
+Canva3.place(y=30, x=720, width=350, height=200)
+
+Canva4 = Canvas(ventana)
+Canva4.place(y=30, x=1080, width=350, height=200)
+
+Canva5 = Canvas(ventana)
+Canva5.place(y=260, x=170, width=800, height=50)
+
+Canva6 = Canvas(ventana)
+Canva6.place(y=315, x=170, width=800, height=50)
+
+Canva7 = Canvas(ventana)
+Canva7.place(y=370, x=170, width=800, height=50)
+
+Canva8 = Canvas(ventana)
+Canva8.place(y=425, x=170, width=800, height=50)
+
+Canva9 = Canvas(ventana)
+Canva9.place(y=260, x=1000, width=400, height=215)
+
+Canva10 = Canvas(ventana)
+Canva10.place(y=500, x=315, width=800, height=50)
+
+Core1 = ScrolledText(Canva1)
+Core1.place(y=0, x=0, width=350, height=200)
+Core2 = Text(Canva2)
+Core2.place(y=0, x=0, width=350, height=200)
+Core3 = Text(Canva3)
+Core3.place(y=0, x=0, width=350, height=200)
+Core4 = Text(Canva4)
+Core4.place(y=0, x=0, width=350, height=200)
+
+
+Cache1 = Text(Canva5)
+Cache1.place(y=0, x=0, width=800, height=50)
+Cache2 = Text(Canva6)
+Cache2.place(y=0, x=0, width=800, height=50)
+Cache3 = Text(Canva7)
+Cache3.place(y=0, x=0, width=800, height=50)
+Cache4 = Text(Canva8)
+Cache4.place(y=0, x=0, width=800, height=50)
+
+Misses = Text(Canva9)
+Misses.place(y=0, x=0, width=400, height=215)
+
+Memory = Text(Canva10)
+Memory.place(y=0, x=0, width=800, height=50)
 
       
-   def run(self):
-      self.root = tk.Tk()
-      self.root.Core1 = tk.Text(self.root)
-      self.root.Core2 = tk.Text(self.root)
-      self.root.Core3 = tk.Text(self.root)
-      self.root.Core4 = tk.Text(self.root)
+def printCore1():
+   while True:
+      f= open("core1.txt", "r")
+      Core1.delete('1.0',END)
+      Core1.insert('1.0',f.read())
+      time.sleep(0.5)
+
+def printCore2():
+   while True:
+      f= open("core2.txt", "r")
+      Core2.delete('1.0',END)
+      Core2.insert('1.0',f.read())
+      time.sleep(0.5)
+
+def printCore3():
+   while True:
+      f= open("core3.txt", "r")
+      Core3.delete('1.0',END)
+      Core3.insert('1.0',f.read())
+      time.sleep(0.5)
+
+def printCore4():
+   while True:
+      f= open("core1.txt", "r")
+      Core4.delete('1.0',END)
+      Core4.insert('1.0',f.read())
+      time.sleep(0.5)
+
+def printMemory():
+   while True:
+      f= open("mem.txt", "r")
+      Memory.delete('1.0',END)
+      Memory.insert('1.0',f.read())
+      time.sleep(0.5)
       
-      self.root.Memory = tk.Text(self.root)
-
-      self.root.Misses = tk.Text(self.root)
-
-      self.root.Cache1 = tk.Text(self.root)
-      self.root.Cache2 = tk.Text(self.root)
-      self.root.Cache3 = tk.Text(self.root)
-      self.root.Cache4 = tk.Text(self.root)
-
-      self.root.title("Modelo de protocolo para coherencia de cache en sistemas multiprocesador")
-      self.root.geometry("1430x600")
-      self.root.resizable(width=False, height=False)
-      self.root.configure(bg = "black")
+def printMisses():
+   while True:
+      f= open("misses.txt", "r")
+      Misses.delete('1.0',END)
+      Misses.insert('1.0',f.read())
+      time.sleep(0.5)
    
-      print(self.p1)
-      self.root.label1= tk.Label(self.root,text="Processors",fg = "Yellow",font=("Helvetica", 14),bg = "black")
-      self.root.label1.place(y=5, x=680,width=100, height=20)
-      
-      self.root.label2= tk.Label(self.root,text="Cache Memory",fg = "Yellow",font=("Helvetica", 14),bg = "black")
-      self.root.label2.place(y=240, x=515,width=150, height=20)
+def printCache1():
+   while True:
+      f= open("cache1.txt", "r")
+      Cache1.delete('1.0',END)
+      Cache1.insert('1.0',f.read())
+      time.sleep(0.5)
 
-      self.root.label2= tk.Label(self.root,text="Cache Misses",fg = "Yellow",font=("Helvetica", 14),bg = "black")
-      self.root.label2.place(y=240, x=1115,width=150, height=20)
+def printCache2():
+   while True:
+      f= open("cache2.txt", "r")
+      Cache2.delete('1.0',END)
+      Cache2.insert('1.0',f.read())
+      time.sleep(0.5)
 
-      self.root.label3= tk.Label(self.root,text=" Principal Memory",fg = "Yellow",font=("Helvetica", 14),bg = "black")
-      self.root.label3.place(y=475, x=650,width=180, height=20)
+def printCache3():
+   while True:
+      f= open("cache3.txt", "r")
+      Cache3.delete('1.0',END)
+      Cache3.insert('1.0',f.read())
+      time.sleep(0.5)
 
-
-      
-      self.root.Core1.place(y=30, x=0, width=350, height=200)
-
-      
-      self.root.Core2.place(y=30, x=360, width=350, height=200)
-
-      
-      self.root.Core3.place(y=30, x=720, width=350, height=200)
-
-      
-      self.root.Core4.place(y=30, x=1080, width=350, height=200)
-
-      
-      self.root.Cache1.place(y=260, x=170, width=800, height=50)
-
-      self.root.Cache2.place(y=315, x=170, width=800, height=50)
-
-
-      self.root.Cache3.place(y=370, x=170, width=800, height=50)
-
-      self.root.Cache4.place(y=425, x=170, width=800, height=50)
-
-      
-      self.root.Misses.place(y=260, x=1000, width=350, height=200)
-
-      
-      self.root.Memory.place(y=500, x=315, width=800, height=50)
+def printCache4():
+   while True:
+      f= open("cache4.txt", "r")
+      Cache4.delete('1.0',END)
+      Cache4.insert('1.0',f.read())
+      time.sleep(0.5)
    
-      self.root.mainloop()
-
-   def printCore(self,ind,param):
-      
-      if ind == "1":
-         print("OCUPO SABER QUE SI ESta LLEGANDO AQUI",ind,param)
-         self.p1 += param+'\n'
-         self.root.Core1.delete('1.0',tk.END)
-         self.root.Core1.insert('end',self.p1)
-      elif ind == "2":
-         self.p2 += param+'\n'
-         self.root.Core2.delete('1.0',tk.END)
-         self.root.Core2.insert('end',self.p2)
-
-      elif ind == "3":
-         self.p3 += param+'\n'
-         self.root.Core3.delete('1.0',tk.END)
-         self.root.Core3.insert('end',self.p3)
-
-      elif ind == "4":
-         self.p4 += param+'\n'
-         self.root.Core4.delete('1.0',tk.END)
-         self.root.Core4.insert('end',self.p4)
-
-   def printMemory(self,mem):
-         self.root.Memory.delete('1.0',tk.END)
-         self.root.Memory.insert('end',self.p4)
-      
-      
-   def printCache(self,ind,param):
-      if ind == "1":
-         self.m1 = "Cache 1" +'\n'+param +'\n'
-         self.root.Cache1.delete('1.0',tk.END)
-         self.root.Cache1.insert('end',self.p4)
-      elif ind == "2":
-         self.m2 = "Cache 2" +'\n'+param +'\n'
-         self.root.Cache2.delete('1.0',tk.END)
-         self.root.Cache2.insert('end',self.p4)
-      elif ind == "3":
-         self.m3 = "Cache 3" +'\n'+param +'\n'
-         self.root.Cache3.delete('1.0',tk.END)
-         self.root.Cache3.insert('end',self.p4)
-      elif ind == "4":
-         self.m4 = "Cache 4" +'\n'+param +'\n'
-         self.root.Cache4.delete('1.0',tk.END)
-         self.root.Cache4.insert('end',self.p4)
-      
-
 
 def main ():
-   app = GUI()
-   a = "prueba"
-   b = "prueba 2"
-   app.start()
-   app.printCore("1",a)
-   app.printCore("1",b)
-   app.printCore("3",a)
-   app.printCache("1",a)
-   app.printCache("1",b)
-   app.root.Cache1.delete("2.0", tk.END)
-   app.root.Cache1.insert(tk.INSERT, '\n' + b)
-   
-   
-##if __name__ == "__main__":
-##   main()
+   hilo1 = Thread(target=printCore1, args=())
+   hilo2 = Thread(target=printCore2, args=())
+   hilo3 = Thread(target=printCore3, args=())
+   hilo4 = Thread(target=printCore4, args=())
+   hilo5 = Thread(target=printMemory, args=())
+   hilo6 = Thread(target=printMisses, args=())
+   hilo7 = Thread(target=printCache1, args=())
+   hilo8 = Thread(target=printCache2, args=())
+   hilo9 = Thread(target=printCache3, args=())
+   hilo10 = Thread(target=printCache4, args=())
+   hilo1.start()
+   hilo2.start()
+   hilo3.start()
+   hilo4.start()
+   hilo5.start()
+   hilo6.start()
+   hilo7.start()
+   hilo8.start()
+   hilo9.start()
+   hilo10.start()
 
+   
+   
+if __name__ == "__main__":
+   main()
+
+ventana.mainloop()
--- Proyecto_Arqui.py
+++ Proyecto_Arqui.py
@@ -1,13 +1,31 @@
 import time
 import threading
-from GUI_Arqui import GUI
 from random import randint
 from collections import deque
 
-gui = GUI()
+core1 = open("core1.txt","w")
+core1.close()
+cache1 = open("cache1.txt","w")
+cache1.close()
+core2 = open("core2.txt","w")
+core2.close()
+cache2 = open("cache2.txt","w")
+cache2.close()
+core3 = open("core3.txt","w")
+core3.close()
+cache3 = open("cache3.txt","w")
+cache3.close()
+core4 = open("core4.txt","w")
+core4.close()
+cache4 = open("cache4.txt","w")
+cache4.close()
+mem = open("mem.txt","w")
+mem.close()
+misses= open("misses.txt","w")
+misses.close()
+
 
 class processor(threading.Thread):
-    global mutex
     
     def __init__(self, name, cache):
         threading.Thread.__init__(self)
@@ -18,27 +36,68 @@ class processor(threading.Thread):
     def write (self):
         pos = randint(0,15)
         data = int(self.name)
-        mutex.acquire()
-        gui.printCore(self.name,"core " + self.name +" escribiendo en:"+str(pos))
-        mutex.release()
+        if self.name == "1":
+            core1 = open ("core1.txt", "a")
+            core1.write('\n' + str("core " + self.name +" escribiendo en:"+str(pos+1)))
+            core1.close()
+        elif self.name == "2":
+            core2 = open ("core2.txt", "a")
+            core2.write('\n' + str("core " + self.name +" escribiendo en:"+str(pos+1)))
+            core2.close()
+        elif self.name == "3":
+            core3 = open ("core3.txt", "a")
+            core3.write('\n' + str("core " + self.name +" escribiendo en:"+str(pos+1)))
+            core3.close()
+        elif self.name == "4":
+            core4 = open ("core4.txt", "a")
+            core4.write('\n' + str("core " + self.name +" escribiendo en:"+str(pos+1)))
+            core4.close()
         self.cache.write(pos,data)
 
     def read (self):
         pos = randint(0,15)
-        mutex.acquire()
-        gui.printCore(self.name,"core " + self.name +" leyendo en:"+str(pos))
-        mutex.release()
+        if self.name == "1":
+            core1 = open ("core1.txt", "a")
+            core1.write('\n' + "core " + self.name +" leyendo en:"+str(pos+1))
+            core1.close()
+        elif self.name == "2":
+            core2 = open ("core2.txt", "a")
+            core2.write('\n' + "core " + self.name +" leyendo en:"+str(pos+1))
+            core2.close()
+        elif self.name == "3":
+            core3 = open ("core3.txt", "a")
+            core3.write('\n' + "core " + self.name +" leyendo en:"+str(pos+1))
+            core3.close()
+        elif self.name == "4":
+            core4 = open ("core4.txt", "a")
+            core4.write('\n' + "core " + self.name +" leyendo en:"+str(pos+1))
+            core4.close()
         data = self.cache.read(pos)
 
     def processing (self):
-        mutex.acquire()
-        gui.printCore(self.name,"core " + self.name +" procesando")        
-        mutex.release()
+        if self.name == "1":
+            core1 = open ("core1.txt", "a")
+            core1.write('\n' + "core " + self.name +" procesando")
+            core1.close()
+        elif self.name == "2":
+            core2 = open ("core2.txt", "a")
+            core2.write('\n' + "core " + self.name +" procesando")
+            core2.close()
+        elif self.name =="3":
+            core3 = open ("core3.txt", "a")
+            core3.write('\n' + "core " + self.name +" procesando")
+            core3.close()
+        elif self.name == "4":
+            core4 = open ("core4.txt", "a")
+            core4.write('\n' + "core " + self.name +" procesando")
+            core4.close()
         time.sleep(1)
 
     def run (self):
         i = 0
         while True:
+            print(i)
+
             i+=1
             aleat = randint(0,2)
 
@@ -71,9 +130,7 @@ class cache(threading.Thread):
         prot = [1,self.name,pos, data]
         time.sleep(0.5)
         bus.bus_princ(prot)
-        mutex.acquire()
-        print ("escritura exitosa in the core " +self.name+",in the position :", pos+1)
-        mutex.release()
+
                    
     def read(self,pos):
         index = self.mem_cache[pos]
@@ -81,15 +138,12 @@ class cache(threading.Thread):
         validate = self.control.read(msi)
         if validate == 0:
             new_data= index[1]
-            mutex.acquire()
-            print ("lectura exitosa in the core "+ self.name+",in the position :", pos+1)
-            mutex.release()
             time.sleep(0.25)
             return new_data
         elif validate == 1:
-            mutex.acquire()
-            print ("cache miss in the core " +self.name+",in the position :", pos+1)
-            mutex.release()
+            misses = open ("misses.txt", "a")
+            misses.write('\n' + "cache miss in the core " +self.name+",in the position :"+str(pos+1))
+            misses.close()
             time.sleep(0.25)
             data = index[1]
             prot = [0,self.name,pos, data]
@@ -98,7 +152,7 @@ class cache(threading.Thread):
                 if self.bus.q_read:
                     break
             while True:
-                read = self.bus.q_read[0]
+                read = self.bus.q_read[-1]
                 if (read[0] == self.name and
                     read[1] == pos):
                     break
@@ -111,9 +165,23 @@ class cache(threading.Thread):
 
     def run (self):
         while True:
-            mutex.acquire()
-            print("memory cache of the core "+ self. name+" " , self.mem_cache)
-            mutex.release()
+            if self.name == "1":
+                cache1 = open ("cache1.txt", "w")
+                cache1.write('\n' +str(self.mem_cache))
+                cache1.close()
+            elif self.name == "2":
+                cache2 = open ("cache2.txt", "w")
+                cache2.write('\n' +str(self.mem_cache))
+                cache2.close()
+            elif self.name == "3":
+                cache3 = open ("cache3.txt", "w")
+                cache3.write('\n' +str(self.mem_cache))
+                cache3.close()
+            elif self.name == "4":
+                cache4 = open ("cache4.txt", "w")
+                cache4.write('\n' + str(self.mem_cache))
+                cache4.close()
+
             time.sleep(0.5)
         
 
@@ -130,37 +198,25 @@ class control_unit(threading.Thread):
     def write(self,msi):
         if msi == 2:
             msi = 0
-            mutex.acquire()
-            print ("valor actualizado de S a M, proceder con la escritura")
-            mutex.release()
+            #valor actualizado de S a M, proceder con la escritura
             return msi
         elif msi == 1:
             msi = 0
-            mutex.acquire()
-            print ("valor actualizado de I a M, proceder con la escritura")
-            mutex.release()
+            #valor actualizado de I a M, proceder con la escritura
             return msi
         else:
-            mutex.acquire()
-            print ("valor actualizado de M a M, proceder con la escritura")
-            mutex.release()
+            #valor actualizado de M a M, proceder con la escritura
             return msi
 
     def read(self,msi):
         if msi == 2:
-            mutex.acquire()
-            print ("proceder con la lectura de cache S a S")
-            mutex.release()
+            #print proceder con la lectura de cache S a S
             return msi
         elif msi== 1:
-            mutex.acquire()
-            print ("cache miss en lectura")
-            mutex.release()
+            #cache miss en lectura
             return msi
         else:
-            mutex.acquire()
-            print ("proceder con la lectura de cache M a M")
-            mutex.release()
+            #proceder con la lectura de cache M a M
             return msi        
 
 
@@ -179,9 +235,7 @@ class control_unit(threading.Thread):
                       self.mem_cache[instr[2]][0] == 2):
                     self.mem_cache[instr[2]] = (2,instr[3])
 
-                
-                
-                    
+                                           
     
 class bus (threading.Thread):
     global mutex
@@ -198,9 +252,6 @@ class bus (threading.Thread):
             pos = instr[2]
             data = instr[3]
             self.memory.write(pos,data)
-            mutex.acquire()
-            print ("successful writing in the bus")
-            mutex.release()
 
     def bus_r (self,instr):
             name = instr[1]
@@ -208,9 +259,6 @@ class bus (threading.Thread):
             data = self.memory.read(pos)
             read = [name,pos,data]
             self.q_read.append(read)
-            mutex.acquire()
-            print ("successful reading in the bus")
-            mutex.release()
 
     def bus_princ (self,prot):
         self.queue.append(prot)     
@@ -253,11 +301,9 @@ class memory (threading.Thread):
 
     def run (self):
         while True:
-            mutex.acquire()
-            print("//////////////////////////////////")
-            print("Memoria Principal " , self.mem_princ)
-            print("//////////////////////////////////")
-            mutex.release()
+            mem = open("mem.txt","w")
+            mem.write('\n' +str(self.mem_princ))
+            mem.close()
             time.sleep(0.5)
 
 mutex = threading.Lock()
@@ -266,8 +312,6 @@ mutex = threading.Lock()
 #Secuencia de inicializaion del programa
 #/////////////////////////////////////////////
 
-#instancia de la interfaz
-gui.start()
 #hilos unicos                   
 princ_mem = memory("principal_memory")
 bus = bus("principal_bus", princ_mem)
@@ -298,5 +342,3 @@ core1.join()
 core2.join()
 core3.join()
 core4.join()
-
-
